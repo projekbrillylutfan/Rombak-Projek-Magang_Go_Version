5ab5b6c31b0224a152b5e93861dee41b050f3e7e
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-11-15 10:16:03 +0700

    alah sia boy

--- test/coba_test.go
+++ test/coba_test.go
@@ -1,20 +1,28 @@
 package test
 
 import (
+	"os"
 	"testing"
 
+	"github.com/joho/godotenv"
 	"github.com/projekbrillylutfan/Rombak-Projek-Magang_Go_Version/configuration"
-	"github.com/stretchr/testify/assert"
 )
 
 
 
-func TestENV(t *testing.T) {
-	config := configuration.New("..env")
+func TestMain(m *testing.M) {
+	// Pastikan path ke .env relatif terhadap root proyek
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic("Error loading .env file for testing")
+	}
+	os.Exit(m.Run())
+}
 
-	assert.Equal(t, "localhost", config.Get("DB_HOST"))
-	assert.Equal(t, "admin", config.Get("DB_USER"))
-	assert.Equal(t, "localhost", config.Get("DB_HOST"))
-	assert.Equal(t, "secret", config.Get("DB_PASSWORD"))
-	assert.Equal(t, 8080, config.Get("SERVER_PORT"))
+func TestConfig(t *testing.T) {
+	config := configuration.New()
+	dbHost := config.Get("DB_HOST")
+	if dbHost == "" {
+		t.Fatal("Expected DB_HOST to be set in environment")
+	}
 }
