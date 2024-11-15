ebf608c429a40bf6552c9206c3a9e4dbe4436d86
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-11-15 14:10:10 +0700

    db init

--- app/database.go
+++ app/database.go
@@ -1,7 +1,64 @@
-package app 
+package app
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
 
+	"github.com/projekbrillylutfan/Rombak-Projek-Magang_Go_Version/configuration"
+	"github.com/projekbrillylutfan/Rombak-Projek-Magang_Go_Version/exception"
+	"golang.org/x/exp/rand"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
 
+func NewDatabase(config configuration.Config) *gorm.DB {
+	username := config.Get("DATASOURCE_USERNAME")
+	password := config.Get("DATASOURCE_PASSWORD")
+	host := config.Get("DATASOURCE_HOST")
+	port := config.Get("DATASOURCE_PORT")
+	dbName := config.Get("DATASOURCE_DB_NAME")
+	maxPoolOpen, err := strconv.Atoi(config.Get("DATASOURCE_POOL_MAX_CONN"))
+	maxPoolIdle, err := strconv.Atoi(config.Get("DATASOURCE_POOL_IDLE_CONN"))
+	maxPollLifeTime, err := strconv.Atoi(config.Get("DATASOURCE_POOL_LIFE_TIME"))
+	exception.PanicLogging(err)
+
+	// Setup logger for GORM
+	loggerDb := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  true,
+		},
+	)
+
+	// Format PostgreSQL connection string
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, dbName)
+
+	// Connect to PostgreSQL using GORM
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: loggerDb,
+	})
+	exception.PanicLogging(err)
+
+	// Configure database connection pooling
+	sqlDB, err := db.DB()
+	exception.PanicLogging(err)
+	sqlDB.SetMaxOpenConns(maxPoolOpen)
+	sqlDB.SetMaxIdleConns(maxPoolIdle)
+	sqlDB.SetConnMaxLifetime(time.Duration(rand.Int31n(int32(maxPollLifeTime))) * time.Millisecond)
+
+	// Uncomment untuk AutoMigrate
+	// err = db.AutoMigrate(&entity.Product{}, &entity.Transaction{}, &entity.TransactionDetail{}, &entity.User{}, &entity.UserRole{})
+	// exception.PanicLogging(err)
+
+	return db
+}
 
 // migrate db
 // migrate -database "postgres://postgres:admin@localhost:5432/rombak_projek_magang_go_version?sslmode=disable" -path db/migrations up
