616f49fd28a63de5a0d0d770aa0f151dfb7e1dfe
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-11-22 09:30:32 +0700

    Fix(Return Coverstion): memperbaiki return convertion agar lebih clean

--- model/web/agenda_model.go
+++ model/web/agenda_model.go
@@ -1,6 +1,10 @@
 package web
 
-import "time"
+import (
+	"time"
+
+	"github.com/projekbrillylutfan/Rombak-Projek-Magang_Go_Version/model/domain"
+)
 
 type AgendaCreateOrUpdate struct {
 	NamaAgenda     string    `json:"nama_agenda" validate:"required"`
@@ -21,4 +25,34 @@ type AgendaModel struct {
 	JenisAcara     *JenisAcaraModel `json:"jenis_acara" validate:"required"`
 	TanggalMulai   time.Time       `json:"tanggal_mulai" validate:"required"`
 	TanggalSelesai time.Time       `json:"tanggal_selesai" validate:"required"`
+}
+
+func ConvertBupatiToModel(bupati *domain.Bupati) *BupatiModel {
+	return &BupatiModel{
+		ID:       bupati.ID,
+		Nama:     bupati.Nama,
+		PeriodeJabatan:  bupati.PeriodeJabatan,
+		CreatedAt: bupati.CreatedAt,
+		UpdateAt: bupati.UpdateAt,
+		// Tambahkan field lainnya jika diperlukan
+	}
+}
+
+func ConvertLokasiToModel(lokasi *domain.Lokasi) *LokasiModel {
+	return &LokasiModel{
+		ID: lokasi.ID,
+		Nama: lokasi.Nama,
+		Alamat: lokasi.Alamat,
+		CreatedAt: lokasi.CreatedAt.String(),
+		UpdateAt: lokasi.UpdateAt.String(),
+	}
+}
+
+func ConvertJenisAcaraToModel(jenisAcara *domain.JenisAcara) *JenisAcaraModel {
+	return &JenisAcaraModel{
+		ID: jenisAcara.ID,
+		NamaJenisAcara: jenisAcara.NamaJenisAcara,
+		CreatedAt: jenisAcara.CreatedAt.String(),
+		UpdateAt: jenisAcara.UpdateAt.String(),
+	}
 }
--- service/impl_service/agenda_service_impl.go
+++ service/impl_service/agenda_service_impl.go
@@ -85,36 +85,6 @@ func (service *AgendaServiceImpl) FindAllAgendaService(ctx context.Context) (res
 	return responses
 }
 
-func ConvertBupatiToModel(bupati *domain.Bupati) *web.BupatiModel {
-	return &web.BupatiModel{
-		ID:       bupati.ID,
-		Nama:     bupati.Nama,
-		PeriodeJabatan:  bupati.PeriodeJabatan,
-		CreatedAt: bupati.CreatedAt,
-		UpdateAt: bupati.UpdateAt,
-		// Tambahkan field lainnya jika diperlukan
-	}
-}
-
-func ConvertLokasiToModel(lokasi *domain.Lokasi) *web.LokasiModel {
-	return &web.LokasiModel{
-		ID: lokasi.ID,
-		Nama: lokasi.Nama,
-		Alamat: lokasi.Alamat,
-		CreatedAt: lokasi.CreatedAt.String(),
-		UpdateAt: lokasi.UpdateAt.String(),
-	}
-}
-
-func ConvertJenisAcaraToModel(jenisAcara *domain.JenisAcara) *web.JenisAcaraModel {
-	return &web.JenisAcaraModel{
-		ID: jenisAcara.ID,
-		NamaJenisAcara: jenisAcara.NamaJenisAcara,
-		CreatedAt: jenisAcara.CreatedAt.String(),
-		UpdateAt: jenisAcara.UpdateAt.String(),
-	}
-}
-
 func (service *AgendaServiceImpl)FindByIdAgendaService(ctx context.Context, id int64) *web.AgendaModel {
 	agenda, err := service.AgendaRepository.FindByIdAgendaRepo(ctx, id)
 	if err != nil {
@@ -125,10 +95,10 @@ func (service *AgendaServiceImpl)FindByIdAgendaService(ctx context.Context, id i
 	return &web.AgendaModel{
 		ID: agenda.IDAgenda,
 		NamaAgenda: agenda.NamaAgenda,
-		Bupati: ConvertBupatiToModel(&agenda.Bupati),
+		Bupati: web.ConvertBupatiToModel(&agenda.Bupati),
 		Deskripsi: agenda.Deskripsi,
-		Lokasi: ConvertLokasiToModel(&agenda.Lokasi),
-		JenisAcara: ConvertJenisAcaraToModel(&agenda.JenisAcara),
+		Lokasi: web.ConvertLokasiToModel(&agenda.Lokasi),
+		JenisAcara: web.ConvertJenisAcaraToModel(&agenda.JenisAcara),
 		TanggalMulai: agenda.TanggalMulai,
 		TanggalSelesai: agenda.TanggalSelesai,
 	}
